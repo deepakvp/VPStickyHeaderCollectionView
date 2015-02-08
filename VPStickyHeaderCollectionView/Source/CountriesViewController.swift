8da3ce23ca36d4a67eda247c0ee2b401dbdce9b0
Deepak VP <deepak.vpd@gmail.com>
2015-02-08 11:33:39 -0800

    Refactoring to update collection view delegate functions as per latest API changes. Also updated comments, copyright and header info

--- VPStickyHeaderCollectionView/Source/CountriesViewController.swift
+++ VPStickyHeaderCollectionView/Source/CountriesViewController.swift
@@ -1,13 +1,26 @@
+// CountriesViewController: .swift
 //
-//  CountriesViewController: .swift
-//  VPStickyHeaderCollectionView
+// Copyright (c) 2014-2015 Deepak VP
 //
-//  Created by Deepak VP on 7/4/14.
-//  Copyright (c) 2014 Deepak VP. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import UIKit
-
 
 class CountriesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
@@ -18,16 +31,16 @@ class CountriesViewController: UIViewController, UICollectionViewDelegate, UICol
     let stickyheaderBackgroundColor = UIColor(red: 219.0/255, green:219.0/255, blue:219.0/255, alpha:0.8)
     let continentHeaderViewHeight = 40.0
 
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Register header suppplementary view
         let stickyHeaderViewNib = UINib(nibName: contientStickyHeaderViewIdentifier, bundle: nil)
-        collectionView.registerNib(stickyHeaderViewNib, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: continentHeaderViewReuseIdentifier)
+        collectionView?.registerNib(stickyHeaderViewNib, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: continentHeaderViewReuseIdentifier)
 
-        // Populate sample data. This will mostly be a call to data manager which eithe returns back with cached data
+        // Populate sample data. This will mostly be a call to data provider which either returns back with cached datas
         // or makes a network request to get fresh data
         populateDataSource()
     }
@@ -43,29 +56,31 @@ class CountriesViewController: UIViewController, UICollectionViewDelegate, UICol
 
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let citiesKeys = Array(citiesInContinent.keys)
-        let continentKeyAtSection : String = citiesKeys[section]
-        let citiesArray : Array = citiesInContinent[continentKeyAtSection]!
-        return citiesArray.count;
+        let continentKeyAtSection = citiesKeys[section]
+        let citiesArray = citiesInContinent[continentKeyAtSection]
+        return citiesArray?.count ?? 0;
     }
 
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         var cell = collectionView.dequeueReusableCellWithReuseIdentifier(cityCellReuseIdentifier, forIndexPath: indexPath) as CityCollectionViewCell
         let citiesKeys = Array(citiesInContinent.keys)
-        let continentKeyAtSection : String = citiesKeys[indexPath.section]
-        let citiesArray : Array = citiesInContinent[continentKeyAtSection]!
-        cell.setUpCellWithCityName(citiesArray[indexPath.row])
+        let continentKeyAtSection = citiesKeys[indexPath.section]
+        let citiesArray = citiesInContinent[continentKeyAtSection]
+        if let cityName = citiesArray?[indexPath.row] {
+            cell.setUpCellWithCityName(cityName)
+        }
         return cell
     }
 
-    func collectionView(collectionView: UICollectionView!, viewForSupplementaryElementOfKind kind: String!, atIndexPath indexPath: NSIndexPath!) -> UICollectionReusableView! {
-        var supplementaryView = collectionView?.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: continentHeaderViewReuseIdentifier, forIndexPath: indexPath) as ContinentStickyHeaderView
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        var supplementaryView = collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionHeader, withReuseIdentifier: continentHeaderViewReuseIdentifier, forIndexPath: indexPath) as ContinentStickyHeaderView
         let keys = Array(citiesInContinent.keys)
-        supplementaryView.setupStickyHeaderView(keys[indexPath!.section])
+        supplementaryView.setupStickyHeaderView(keys[indexPath.section])
         return supplementaryView
     }
 
     // MARK: UICollectionViewDelegateFlowLayout methods
-    func collectionView(collectionView: UICollectionView!, layout collectionViewLayout: UICollectionViewLayout!, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSizeMake(collectionView.frame.size.width, CGFloat(continentHeaderViewHeight))
     }
 
--- VPStickyHeaderLayout/VPStickyHeaderCollectionViewLayout.swift
+++ VPStickyHeaderLayout/VPStickyHeaderCollectionViewLayout.swift
@@ -1,23 +1,48 @@
+// VPStickyHeaderCollectionViewLayout.swift
 //
-//  VPStickyHeaderCollectionViewLayout.swift
-//  VPStickyHeaderCollectionView
+// Copyright (c) 2014-2015 Deepak VP
 //
-//  Created by Deepak VP on 7/4/14.
-//  Copyright (c) 2014 Deepak VP. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import UIKit
 
-let stickyHeaderZIndex = 100
-
+/// A custom collection view layout to overrides the default behaviour of supplementary view
+/// and provides a sticky header implementation. The implementation overrides the frame layout attribute
+/// of supplementary and keeps it at positioned at the top. This handles sticky header in both vertical and
+/// horizontal scrolling collectionview
+///
+/// TODO: Adding all indexPaths in layoutAttributesForElementsInRect might result in poor performance
+/// if the collection view is large. This can be optimized to only add indexPaths which needs to be in the rect
 class VPStickyHeaderCollectionViewLayout: UICollectionViewFlowLayout {
 
-    var stickyHeaderIndexPaths = [NSIndexPath]()
+    /// Specifies the z index of the supplementary view so that they can on top of cells and decoration views
+    private let stickyHeaderZIndex = 1
 
+    /// stores the sticky header indexpaths
+    private var stickyHeaderIndexPaths = [NSIndexPath]()
+
+    // MARK - UICollectionViewLayout method overrides
     override func prepareLayout()  {
         super.prepareLayout()
 
-        stickyHeaderIndexPaths.removeAll(keepCapacity: true)
+        // Identifies all the section which needs a sticky supplementary header view.
+        stickyHeaderIndexPaths.removeAll(keepCapacity: false)
         if let numberOfSections = collectionView?.numberOfSections() {
             for index in 0 ..< numberOfSections {
                 var indexPath = NSIndexPath(forItem: 0, inSection: index)
@@ -27,10 +52,14 @@ class VPStickyHeaderCollectionViewLayout: UICollectionViewFlowLayout {
     }
 
     override func shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) -> Bool {
+        // For every change in bounds we need to invalidate the layout, so that we can override the
+        // supplementary frame attribute
         return true
     }
 
     override func layoutAttributesForElementsInRect(rect: CGRect) -> [AnyObject]? {
+        // In layoutAttributesForElementsInRect, we need to get the indexpaths within the rect 
+        // and add all header indexPath
         var attributesInRect = super.layoutAttributesForElementsInRect(rect)
         var updatedAttributesInRect = [UICollectionViewLayoutAttributes]()
 
@@ -50,37 +79,42 @@ class VPStickyHeaderCollectionViewLayout: UICollectionViewFlowLayout {
             updatedAttributesInRect.append(headerAttribute)
         }
 
+        // Returning attributes for cell and all sticky supplmentary header views
         return updatedAttributesInRect
     }
 
     override func layoutAttributesForSupplementaryViewOfKind(elementKind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes! {
-        var layoutAttribute : UICollectionViewLayoutAttributes = super.layoutAttributesForSupplementaryViewOfKind(elementKind, atIndexPath: indexPath)
+        let layoutAttribute  = super.layoutAttributesForSupplementaryViewOfKind(elementKind, atIndexPath: indexPath)
         let contentOffset = collectionView?.contentOffset ?? CGPointZero
-        var nextHeaderOrigin : CGPoint = CGRect.infiniteRect.origin
-        var nextHeaderIndex : Int;
+        var nextHeaderOrigin = CGPointZero
+        var nextHeaderIndex : Int = 0
 
+        // Get the layout attribute of next supplementary index and use that to modify current header view
+        // to get the next header pushing out the current sticky header
         if (indexPath.section + 1 < collectionView?.numberOfSections()) {
             nextHeaderIndex = indexPath.section + 1
-            let nextIndexPath : NSIndexPath = NSIndexPath(forItem: 0, inSection: nextHeaderIndex)
-            var nextHeaderFrame : CGRect = super.layoutAttributesForSupplementaryViewOfKind(elementKind, atIndexPath: nextIndexPath).frame
+            let nextIndexPath = NSIndexPath(forItem: 0, inSection: nextHeaderIndex)
+            var nextHeaderFrame = super.layoutAttributesForSupplementaryViewOfKind(elementKind, atIndexPath: nextIndexPath).frame
             nextHeaderOrigin = nextHeaderFrame.origin
         }
         else {
             return layoutAttribute;
         }
 
-        var headerFrame : CGRect = layoutAttribute.frame
+        // Override header origin
+        var headerFrame = layoutAttribute.frame
         if (scrollDirection == UICollectionViewScrollDirection.Vertical) {
-            var nextStickyCellY : CGFloat = nextHeaderOrigin.y - headerFrame.size.height
-            var currentStickyCellY : CGFloat = max(contentOffset.y, headerFrame.origin.y)
+            var nextStickyCellY = nextHeaderOrigin.y - headerFrame.size.height
+            var currentStickyCellY = max(contentOffset.y, headerFrame.origin.y)
             headerFrame.origin.y = min(currentStickyCellY, nextStickyCellY)
         }
         else {
-            var nextStickyCellX : CGFloat = nextHeaderOrigin.x - headerFrame.size.width
+            var nextStickyCellX = nextHeaderOrigin.x - headerFrame.size.width
             var currentStickyCellX = max(contentOffset.x, headerFrame.origin.x)
             headerFrame.origin.x = min(currentStickyCellX, nextStickyCellX)
         }
 
+        // Set the zIndex of the supplementary view so that it can stay on top of other elements in collection view
         layoutAttribute.zIndex = stickyHeaderZIndex
         layoutAttribute.frame = headerFrame
         return layoutAttribute
